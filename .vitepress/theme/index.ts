05016271a5f583de1876beb9f7b20a334aae57e3
PasseRR <xie__hai@sina.com>
2023-10-11 10:08:24 +0800

    plantmul 修改

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -26,7 +26,7 @@ export default {
     setup() {
         const route = useRoute()
         const initZoom = () => {
-            // mediumZoom('[data-zoomable]', {background: 'var(--vp-c-bg)'});
+            mediumZoom('[data-zoomable]', {background: 'var(--vp-c-bg)'});
             mediumZoom('.main img', {background: 'var(--vp-c-bg)'});
         };
         onMounted(() => initZoom());
