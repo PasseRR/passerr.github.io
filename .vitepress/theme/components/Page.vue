a5913242b01ee566cf9eacaab0f51d11f8b9e39b
PasseRR <xie__hai@sina.com>
2025-06-30 18:03:18 +0800

    新窗口打开页面icon问题

--- .vitepress/theme/components/Page.vue
+++ .vitepress/theme/components/Page.vue
@@ -2,7 +2,7 @@
   <ol>
     <li v-for="(article, index) in posts" :key="index" class="post-list">
       <div class="post-title">
-        <a :href="withBase(article.regularPath) + '.html'" class="hover-underline-animation" target="_blank">
+        <a :href="withBase(article.regularPath) + '.html'" class="hover-underline-animation no-icon" target="_blank">
           {{ article.frontMatter.title }}
           <span class="date"><span class="fa fa-clock-o"></span>{{ article.frontMatter.date }}</span>
         </a>
